d5b91ce14ea7796d68df690f64e50b78e2d711d8
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-03 16:54:58 +0300

    any project

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import { Route, Switch } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
-import Redirect from "react-router-dom/es/Redirect";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
 
 function App() {
   return (
-    <div>
-      <div>
-        <MainHeader />
-        <main>
-          <Switch>
-            <Route path="/" exact>
-              <Redirect to="/welcome" />
-            </Route>
-            <Route path="/welcome">
-              <Welcome />
-            </Route>
-            <Route path="/products" exact>
-              <Products />
-            </Route>
-            <Route path="/products/:productId">
-              <ProductDetail />
-            </Route>
-          </Switch>
-        </main>
-      </div>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,17 @@
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+
+const QuoteDetail = () => {
+  const params = useParams();
+  return (
+    <>
+      <h1>QuoteDetail</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
+};
+
+export default QuoteDetail;
