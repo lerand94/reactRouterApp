090b6ee5d3bae2d8d540d62dfbe8f5a78ba6619d
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-03 20:34:01 +0300

    added 404 route, and used useHistory as alternative aprroach to redirect user

--- src/components/quotes/QuoteItem.js
+++ src/components/quotes/QuoteItem.js
@@ -10,7 +10,7 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <Link className="btn" to={`/qoutes/${props.id}`}>
+      <Link className="btn" to={`/quotes/${props.id}`}>
         View Fullscreen
       </Link>
     </li>
