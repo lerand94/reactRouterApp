b9853f543d6206adc0a9b1dfcd1f306c3773fa38
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-03 15:43:22 +0300

    installed react router package. Created 2 cmp and used 2 route for displayed any pages

---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+const Products = () => {
+  return <h2>Let's products started!</h2>;
+};
+
+export default Products;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,5 @@
+const Products = () => {
+  return <h2>Let's welcome started!</h2>;
+};
+
+export default Products;
